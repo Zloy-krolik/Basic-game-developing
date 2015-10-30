a156915c5be1f1b82c597cce17542f132542423f
Zloy-krolik <webdev56@gmail.com>
2015-10-30 14:20:02 +0300

    Create GamePanel.java

---
+++ GamePanel.java
@@ -0,0 +1,128 @@
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+
+public class GamePanel extends JPanel 
+	implements Runnable, KeyListener {
+	
+	public static int WIDTH = 400;
+	public static int HEIGHT = 400;
+	
+	private Thread thread;
+	private boolean running;
+	
+	private BufferedImage image;
+	private Graphics2D g;
+	
+	private int FPS = 30;
+	private double averageFPS;
+	
+	private Player player;
+	
+	public GamePanel() {
+		super();
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		setFocusable(true);
+		requestFocus();
+	}
+	
+	public void addNotify() {
+		super.addNotify();
+		if(thread == null) {
+			thread = new Thread(this);
+			thread.start();
+		}
+		addKeyListener(this);
+	}
+	
+	public void run() {
+		
+		running = true;
+
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		g = (Graphics2D) image.getGraphics();
+		
+		player = new Player();
+		
+		long startTime;
+		long URDTimeMillis;
+		long waitTime;
+		long totalTime = 0;
+		
+		int frameCount = 0;
+		int maxFrameCount = 30;
+		
+		long targetTime = 1000 / FPS;
+		
+		while(running) {
+			
+			startTime = System.nanoTime();
+			
+			gameUpdate();
+			gameRender();
+			gameDraw();
+			
+			URDTimeMillis = (System.nanoTime() - startTime) / 1000000;
+			waitTime = targetTime - URDTimeMillis;
+			
+			try {
+				Thread.sleep(waitTime);
+			}
+			catch(Exception e) {
+				
+			}
+			totalTime += System.nanoTime() - startTime;
+			frameCount++;
+			if(frameCount == maxFrameCount) {
+				averageFPS = 1000.0 / ((totalTime / frameCount) / 1000000);
+				frameCount = 0;
+				totalTime = 0;
+			}
+		}
+	}
+	
+	private void gameUpdate() {
+		
+		player.update();
+	}
+	
+	private void gameRender() {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.setColor(Color.BLACK);
+		g.drawString("FPS: " + averageFPS, 200, 200);
+		
+		player.draw(g);
+	}
+	
+	private void gameDraw() {
+		Graphics g2 = this.getGraphics();
+		g2.drawImage(image, 0, 0, null);
+		g2.dispose();
+	}
+	
+	public void keyTyped(KeyEvent key) {}
+	public void keyPressed(KeyEvent key) {
+		int keyCode = key.getKeyCode();
+		if(keyCode == KeyEvent.VK_LEFT)
+			player.setLeft(true);
+		if(keyCode == KeyEvent.VK_RIGHT)
+			player.setRight(true);
+		if(keyCode == KeyEvent.VK_UP)
+			player.setUp(true);
+		if(keyCode == KeyEvent.VK_DOWN)
+			player.setDown(true);
+	}
+	public void keyReleased(KeyEvent key) {
+		int keyCode = key.getKeyCode();
+		if(keyCode == KeyEvent.VK_LEFT)
+			player.setLeft(false);
+		if(keyCode == KeyEvent.VK_RIGHT)
+			player.setRight(false);
+		if(keyCode == KeyEvent.VK_UP)
+			player.setUp(false);
+		if(keyCode == KeyEvent.VK_DOWN)
+			player.setDown(false);
+	}
+}
